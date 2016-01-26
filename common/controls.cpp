499796bc61d68b04da4371f08de0172b6e756842
nem <sks156324@gmail.com>
2016-01-26 17:20:08 +0900

    fix some code

--- common/controls.cpp
+++ common/controls.cpp
@@ -54,7 +54,7 @@ void computeMatricesFromInputs(){
 	glfwSetCursorPos(window, 1024/2, 768/2);
 
 	// Compute new orientation
-	horizontalAngle -= mouseSpeed * float(1024/2 - xpos );
+	horizontalAngle += mouseSpeed * float(1024/2 - xpos );
 	verticalAngle   += mouseSpeed * float( 768/2 - ypos );
 
 	// Direction : Spherical coordinates to Cartesian coordinates conversion
